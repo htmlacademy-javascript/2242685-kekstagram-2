edaa67a575c431936f977f298c720b2f4b1d7601
sergs1305 <sergs13@gmail.com>
2024-10-15 18:51:24 +0300

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -27,3 +27,18 @@ function extractNumbers (stringToExtract) {
   return parseInt(numbersString, 10);
 }
 //console.log(extractNumbers(' d1f23 gh4@5j'));
+
+function isWorkingHours (startTime, endTime, meetingStartTime, meetingDuration) { // Время - строка в формате часы:минуты. meetingDuration - число в минутах
+  const startTimeInMinutes = timeToMinutes(startTime);
+  const endTimeInMinutes = timeToMinutes(endTime);
+  const meetingStartTimeInMinutes = timeToMinutes(meetingStartTime);
+  return (meetingStartTimeInMinutes >= startTimeInMinutes) && (meetingStartTimeInMinutes + meetingDuration <= endTimeInMinutes);
+  //console.log(endTimeInMinutes);
+}
+
+function timeToMinutes (timeInHours) { // время с полуночи в минутах
+  const timeArray = timeInHours.split(':'); // '8:30' -> ['8', '30']
+  return Number(timeArray[0]) * 60 + Number(timeArray[1]);
+}
+
+isWorkingHours('8:30', '17:30', '12:30', 300);
