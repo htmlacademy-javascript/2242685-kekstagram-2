8b49e52fae720dc644277966b18acf547fc806bb
sergs1305 <sergs13@gmail.com>
2024-10-22 17:25:01 +0300

    Отрисуй меня полностью

---
+++ js/thumbnails-rendering.js
@@ -0,0 +1,32 @@
+/*
+<a href="#" class="picture">
+<img class="picture__img" src="" width="182" height="182" alt="Случайная фотография">
+<p class="picture__info">
+  <span class="picture__comments"></span>
+  <span class="picture__likes"></span>
+</p>
+</a>
+*/
+
+import {getPhotosData} from './data.js';
+const photosData = getPhotosData();
+
+let picturesContainer = document.querySelector('.pictures');
+let pictureTemplate = document.querySelector('#picture').content;
+const picturesFragment = document.createDocumentFragment();
+
+for (let i = 0; i < photosData.length; i++) {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  const pictureImg = pictureElement.querySelector('img');
+  pictureImg.src = photosData[i].url;
+  pictureImg.alt = photosData[i].description;
+  const pictureComments = pictureElement.querySelector('.picture__comments');
+  pictureComments.textContent = photosData[i].comments.length;
+  const pictureLikes = pictureElement.querySelector('.picture__likes');
+  pictureLikes.textContent = photosData[i].likes;
+
+  picturesFragment.append(pictureElement);
+}
+
+picturesContainer.append(picturesFragment);
