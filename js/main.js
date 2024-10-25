1a519301b9528058355283a7e609679f9d9b2632
sergs1305 <sergs13@gmail.com>
2024-10-25 15:43:52 +0300

    1025_2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ import {thumbnailsRendering} from './thumbnails-rendering.js';
 import {onThumbnailClick} from './big-pictures-rendering.js';
 
 const photosData = getPhotosData();
+
+// var someVar = (function(){
+//        //...
+//        return result;
+//     }());
+
 thumbnailsRendering(photosData);
 
 //document.querySelector('.pictures').addEventListener('click', onThumbnailClick);
