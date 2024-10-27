eb33a64cc050a03cdccf926f7e91f7e0fddb92fe
sergs1305 <sergs13@gmail.com>
2024-10-27 18:33:51 +0300

    1027

--- js/main.js
+++ js/main.js
@@ -3,14 +3,8 @@ import {thumbnailsRendering} from './thumbnails-rendering.js';
 import {onThumbnailClick} from './big-pictures-rendering.js';
 
 const photosData = getPhotosData();
-
-// var someVar = (function(){
-//        //...
-//        return result;
-//     }());
+//console.log(photosData);
 
 thumbnailsRendering(photosData);
 
-//document.querySelector('.pictures').addEventListener('click', onThumbnailClick);
-//document.querySelector('.pictures').addEventListener("click", onThumbnailClick(photosData));
 document.querySelector('.pictures').addEventListener("click", event => onThumbnailClick(event, photosData));
