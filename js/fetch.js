0a126ace2b7dab2d89a42428a9bc55662b11654b
sergs1305 <sergs13@gmail.com>
2024-11-06 20:42:36 +0300

    fetch 4 ) готово!

--- js/fetch.js
+++ js/fetch.js
@@ -2,18 +2,20 @@ import {thumbnailsRendering} from './thumbnails-rendering.js';
 import {bigPicturesRendering} from './big-pictures-rendering.js';
 import {closeForm, onFormKeydown} from './form.js';
 
-const URL = 'https://31.javascript.htmlacademy.pro/kekstagram/data';
+const URL_TO_GET = 'https://31.javascript.htmlacademy.pro/kekstagram/data';
+const URL_TO_POST = 'https://31.javascript.htmlacademy.pro/kekstagram';
 const bodyElement = document.querySelector('body');
 const submitButton = document.querySelector('#upload-submit');
 const submitButtonText = {
   IDLE: 'Опубликовать',
   SENDING: 'Публикую...'
 };
+let result = '';
 
 function getPhotosData () {
   // 4.1. Загрузка изображений от других пользователей производится сразу после открытия страницы
   // с удалённого сервера: https://31.javascript.htmlacademy.pro/kekstagram/data.
-  fetch(URL)
+  fetch(URL_TO_GET)
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -55,7 +57,7 @@ function postPhotoData(evt) {
   const formData = new FormData(evt.target);
   blockSubmitButton();
   fetch(
-    'https://31.javascript.htmlacadem.pro/kekstagram',
+    URL_TO_POST,
     {
       method: 'POST',
       body: formData,
@@ -63,86 +65,48 @@ function postPhotoData(evt) {
   )
     .then(() => {
       closeForm();
-      onPostDataSuccess();
+      result = 'success';
+      onPostData();
     })
     .catch(() => {
-      onPostDataError();
+      result = 'error';
+      onPostData();
     })
     .finally(unblockSubmitButton);
 }
 
-function onPostDataSuccess() {
-  // 3.4. Если отправка данных прошла успешно, показывается соответствующее сообщение.
-  // Разметку сообщения, которая находится в блоке #success внутри шаблона template, нужно разместить перед закрывающим тегом </body>.
-  // Сообщение должно удаляться со страницы после нажатия на кнопку .success__button,
-  //  по нажатию на клавишу Esc и по клику на произвольную область экрана за пределами блока с сообщением.
-
-  const dataSuccessTemplate = document.querySelector('#success').content;
-  const dataSuccessFragment = dataSuccessTemplate.cloneNode(true);
-  bodyElement.append(dataSuccessFragment);
-  const successButton = bodyElement.querySelector('.success__button');
-  successButton.addEventListener('click', closeSuccessWindow);
-  document.addEventListener('keydown', onDocumentKeydownSuccess);
-  document.addEventListener('click', onDocumentClickSuccess);
-}
-
-function onDocumentClickSuccess (evt) {
-  if (evt.target.matches('.success')) { //если клик не на окне сообщения
-    closeSuccessWindow();
-  }
-}
-
-function onDocumentKeydownSuccess (evt) {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    closeSuccessWindow();
-  }
-}
-
-function closeSuccessWindow () {
-  document.removeEventListener('keydown', onDocumentKeydownSuccess);
-  document.removeEventListener('click', onDocumentClickSuccess);
-  bodyElement.querySelector('.success').remove();
-}
-
-function onPostDataError() {
-  // 3.5. Если при отправке данных произошла ошибка запроса, нужно показать соответствующее сообщение.
-  // Разметку сообщения, которая находится в блоке #error внутри шаблона template, нужно разместить перед закрывающим тегом </body>.
-  // Сообщение должно удаляться со страницы после нажатия на кнопку .error__button,
-  //  по нажатию на клавишу Esc и по клику на произвольную область экрана за пределами блока с сообщением.
-  // В таком случае вся введённая пользователем информация сохраняется, чтобы у него была возможность отправить форму повторно.
-
-  const dataErrorTemplate = document.querySelector('#error').content;
-  const dataErrorFragment = dataErrorTemplate.cloneNode(true);
-  bodyElement.append(dataErrorFragment);
-  const errorButton = bodyElement.querySelector('.error__button');
-  errorButton.addEventListener('click', closeErrorWindow);
+function onPostData() {
+  const messageTemplate = document.querySelector(`#${result}`).content;
+  const messageFragment = messageTemplate.cloneNode(true);
+  bodyElement.append(messageFragment);
   // удалить обработчик Escape для формы
   document.removeEventListener('keydown', onFormKeydown);
-  document.addEventListener('keydown', onDocumentKeydownError);
-  document.addEventListener('click', onDocumentClickError);
-
+  document.addEventListener('keydown', onMessageWindowKeydown);
+  document.addEventListener('click', onMessageWindowClick);
 }
 
-function onDocumentKeydownError (evt) {
+function onMessageWindowClick (evt) {
+  if (evt.target.matches(`.${result}`) || evt.target.matches(`.${result}__button`)) {
+    closeMessageWindow();
+  }
+}
+
+function onMessageWindowKeydown (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    closeErrorWindow();
+    closeMessageWindow();
   }
 }
 
-function onDocumentClickError (evt) {
-  if (evt.target.matches('.error')) { //если клик не на окне сообщения
-    closeErrorWindow();
+function closeMessageWindow () {
+  document.removeEventListener('keydown', onMessageWindowKeydown);
+  if (result === 'error') {
+    // добавить обработчик Escape для формы
+    document.addEventListener('keydown', onFormKeydown);
   }
-}
+  document.removeEventListener('click', onMessageWindowClick);
+  bodyElement.querySelector(`.${result}`).remove();
 
-function closeErrorWindow () {
-  document.removeEventListener('keydown', onDocumentKeydownError);
-  // добавить обработчик Escape для формы
-  document.addEventListener('keydown', onFormKeydown);
-  document.removeEventListener('click', onDocumentClickError);
-  bodyElement.querySelector('.error').remove();
 }
 
 export {getPhotosData, postPhotoData};
