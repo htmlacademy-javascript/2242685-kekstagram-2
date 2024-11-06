838bcf0e30864ccafd0da600cb49e6a70025218b
Keks <keks@htmlacademy.ru>
2024-11-06 20:45:17 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,15 @@
+// import {getPhotosData} from './data.js';
+// import {thumbnailsRendering} from './thumbnails-rendering.js';
+// import {bigPicturesRendering} from './big-pictures-rendering.js';
+import {loadingPictureForm} from './form.js';
+import {getPhotosData} from './fetch.js';
+
+getPhotosData();
+
+// const photosData = getPhotosData();
+
+// thumbnailsRendering(photosData);
+
+// bigPicturesRendering(photosData);
+
+loadingPictureForm();
